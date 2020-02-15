f1ee8a068230c7478b9374925aba2e9382bb0b42
magix <magixer@kali>
2020-02-15 17:52:32 -0500

    Initial Commit

--- host_disc+scan.sh
+++ host_disc+scan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#scans hosts from 10.11.1.0-255 and pipes alive ones to nmap with default script and version detection...
 for hosts in $(seq 0 255);do 
 	for ports in $(ping -c 1 10.11.1.$hosts |grep "bytes from" |cut -d" " -f 4|cut -d":" -f1  &);do
 		nmap -sC -sV $ports -oG ~/Desktop/msc/10.11.1.$hosts &
